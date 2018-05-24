02389bcb30e5c0a037142c29a81d90623cc0ffbc
Andrew Davis <tuf75588@temple.edu>
2018-05-23 22:51:22 -0400

    update css styles and dom nodes

--- client/main.js
+++ client/main.js
@@ -1,25 +1,56 @@
 const API_URL = "https://server-flpaxndxrd.now.sh/videos";
 const videosElement = document.querySelector("#videos");
+const filterInput = document.querySelector('#filter');
+let allVideos;
+
+filterInput.addEventListener('keyup', filterList);
+
+function filterList(event) {
+  const filter = event.target.value;
+  if (allVideos) {
+    const regExp = new RegExp(filterInput.value, 'gi')
+    allVideos.forEach(video => {
+      if (video.snippet.title.match(regExp)) {
+        //show video
+        console.log(video)
+      } else {
+        //hide the video
+      }
+    })
+  }
+}
+
 fetch(API_URL)
   .then(response => response.json())
   .then(videos => {
-    console.log("====================================");
-    console.log(videos);
-    console.log("====================================");
+    allVideos = videos
+
     videos.forEach(video => {
+      const colDiv = document.createElement('div');
+      colDiv.className = 'col-4'
+
       const videoElement = document.createElement("div");
-      videoElement.className = "media";
+      videoElement.className = "card ma-1";
+
+      colDiv.appendChild(videoElement)
+
       const img = document.createElement("img");
+      const imageRes = video.snippet.thumbnails.standard || video.snippet.thumbnails.medium || video.snippet.thumbnails.high;
+
+
       img.src = video.snippet.thumbnails.high.url;
-      img.className = "mr-3";
+      img.className = "card-img-top";
       videoElement.appendChild(img);
+
       const mediaBody = document.createElement("div");
-      mediaBody.className = "media-body";
+      mediaBody.className = "card-body";
       videoElement.appendChild(mediaBody);
+
       const h5 = document.createElement("h5");
-      h5.className = "mt-0";
+      h5.className = "card-title";
       h5.textContent = video.snippet.title;
+
       mediaBody.appendChild(h5);
-      videosElement.appendChild(videoElement);
+      videosElement.appendChild(colDiv);
     });
   });
